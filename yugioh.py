92c982781a99f3bcf60297f6f634fa70c74cfc0b
rheber <rheber@rediffmail.com>
2022-04-03 15:17:41 +1000

    update yugioh api to use v7

--- yugioh.py
+++ yugioh.py
@@ -1,7 +1,7 @@
 from shared import loadJson, saveUrlToFile
 
 def cardsUrl(banlist="tcg"):
-    return "https://db.ygoprodeck.com/api/v5/cardinfo.php?banlist={}&sort=name".format(banlist)
+    return "https://db.ygoprodeck.com/api/v7/cardinfo.php?banlist={}&sort=name".format(banlist)
 
 def saveCardsToFile(banlist="tcg"):
     saveUrlToFile(cardsUrl(banlist), "yugioh-{}.json".format(banlist))
@@ -10,6 +10,6 @@ def getCardImageUrl(card):
     return card["card_images"][0]["image_url"]
 
 #saveCardsToFile()
-#cards = loadJson("yugioh-tcg.json")
+#cards = loadJson("yugioh-tcg.json")["data"]
 #for card in cards:
 #    saveUrlToFile(getCardImageUrl(card), "yugioh/{}-{}.jpg".format(card["id"], card["name"]), "wb")
